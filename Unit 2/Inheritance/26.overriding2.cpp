2b0ed31a0e7324104ba8eae06143b2aa8af517b1
karvind08 <karvind08@gmail.com>
2023-03-08 22:13:45 +0530

    8 March 2023

---
+++ Unit 2/Inheritance/26.overriding2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Father
+{
+public:
+    void display()
+    {
+        cout << "Bajaj Scooter" << endl;
+    }
+};
+class Son : public Father
+{
+public:
+    void display()
+    {
+        cout << "Pulsor Bike" << endl;
+    }
+};
+int main()
+{
+    Son S1;
+    S1.display();
+    S1.Father::display();
+    return (0);
+}
