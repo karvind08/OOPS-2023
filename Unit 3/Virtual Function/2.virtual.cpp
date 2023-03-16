a9f7d5f39616dbe292f4ae400ae17b6e9f77d1a6
karvind08 <karvind08@gmail.com>
2023-03-16 19:55:42 +0530

    16 March 2023

---
+++ Unit 3/Virtual Function/2.virtual.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void display()
+    {
+        cout << "Display in A" << endl;
+    }
+    virtual void show()
+    {
+        cout << "Show in A" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void display()
+    {
+        cout << "Display in B" << endl;
+    }
+    void show()
+    {
+        cout << "Show in B" << endl;
+    }
+};
+int main()
+{
+    A A1, *P;
+    B B1;
+    P = &A1;
+    P->display();
+    P->show();
+    cout << "----------" << endl;
+    P = &B1;
+    P->display();
+    P->show();
+    return (0);
+}
---
+++ Unit 3/Virtual Function/5.virtual.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    virtual void display()
+    {
+        cout << "Display in A" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void display()
+    {
+        cout << "Display in B" << endl;
+    }
+};
+int main()
+{
+    A A1, *P;
+    B B1;
+    P = &B1;
+    P->display();
+}
