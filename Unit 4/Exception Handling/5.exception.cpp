965881835e402b6ecede3f219e8c31e95db79a7a
karvind08 <karvind08@gmail.com>
2023-03-26 15:53:27 +0530

    26 March 2023

---
+++ Unit 4/Exception Handling/5.exception.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "I am in Main" << endl;
+    try
+    {
+        cout << "I am in try" << endl;
+    }
+    catch (int e)
+    {
+        cout << "I am in catch: " << e << endl;
+    }
+    cout << "The End" << endl;
+    return (0);
+}
