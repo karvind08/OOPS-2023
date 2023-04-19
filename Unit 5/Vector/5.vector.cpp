a026b199ca47bbd7f2e7aa9ec66f956857a00a38
karvind08 <karvind08@gmail.com>
2023-04-19 21:15:28 +0530

    19 April 2023

--- Unit 5/Vector/5.vector.cpp
+++ Unit 5/Vector/5.vector.cpp
@@ -8,16 +8,16 @@ int main()
     vector<vector<int>> v{{1, 0, 1}, {0, 1, 3}, {1, 0, 1}};
 
     // Iterator for the 2-D vector
-    vector<vector<int>>::iterator it1;
+    vector<vector<int>>::iterator i1;
 
     // Iterator for each vector inside the 2-D vector
-    vector<int>::iterator it2;
+    vector<int>::iterator i2;
 
     // Traversing a 2-D vector using iterators
-    for (it1 = v.begin(); it1 != v.end(); it1++)
+    for (i1 = v.begin(); i1 != v.end(); i1++)
     {
-        for (it2 = it1->begin(); it2 != it1->end(); it2++)
-            cout << *it2 << " ";
+        for (i2 = i1->begin(); i2 != i1->end(); i2++)
+            cout << *i2 << " ";
         cout << endl;
     }
 }
