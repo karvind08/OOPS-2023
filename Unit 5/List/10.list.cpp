778f9f8a5fd2f0cab5453a42ff65625f574ff367
karvind08 <karvind08@gmail.com>
2023-04-23 20:05:40 +0530

    23 April 2023

--- Unit 5/List/10.list.cpp
+++ Unit 5/List/10.list.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     list<int> L1 = {10, 2, 43, 14};
-    L1.sort();
+    L1.reverse();
     for (auto i = L1.begin(); i != L1.end(); i++)
         cout << *i << " ";
     return (0);
