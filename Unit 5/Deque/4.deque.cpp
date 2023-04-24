cb9ae214824cfa9a1fbc2f8413024449aff3e113
karvind08 <karvind08@gmail.com>
2023-04-24 21:53:58 +0530

    24 April 2023

---
+++ Unit 5/Deque/4.deque.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+int main()
+{
+    deque<int> D = {1, 2, 3, 4, 5};
+    deque<int>::iterator i;
+    for (i = D.begin(); i != D.end(); i++)
+        cout << *i << " ";
+    D.push_back(10);
+    cout << "\nAfter adding element after push back" << endl;
+    for (i = D.begin(); i != D.end(); i++)
+        cout << *i << " ";
+    return (0);
+}
