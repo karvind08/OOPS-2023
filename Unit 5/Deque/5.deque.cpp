9899220e14900a96f90150d1a966d767eb09e12e
karvind08 <karvind08@gmail.com>
2023-04-24 21:57:47 +0530

    24 April 2023

---
+++ Unit 5/Deque/5.deque.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+int main()
+{
+    deque<int> D = {1, 2, 3, 4, 5};
+    deque<int>::iterator i;
+    for (i = D.begin(); i != D.end(); i++)
+        cout << *i << " ";
+    cout << "\nThe size of " << D.size();
+    return (0);
+}
