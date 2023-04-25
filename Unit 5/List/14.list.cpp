e9e2c72d1d2fd6e9e947e0aa73abd998c64937aa
karvind08 <karvind08@gmail.com>
2023-04-25 06:36:54 +0530

    25 April 2023

---
+++ Unit 5/List/14.list.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
+    list<int> L1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    list<int>::iterator i;
+    for (i = L1.begin(); i != L1.end(); i++)
+    {
+        if (*i % 2 == 1)
+            L1.erase(i);
+    }
+    cout << "\nThe List of odd elements" << endl;
+    for (auto i = L1.begin(); i != L1.end(); i++)
+        cout << *i << " ";
+    return (0);
+}
