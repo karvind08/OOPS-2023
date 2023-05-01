f6d7ae3e7f7d29202d14a811bcb72e674b99f4f7
karvind08 <karvind08@gmail.com>
2023-05-01 20:18:44 +0530

    1 May 2023

---
+++ Unit 5/Function Object/3.fo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <functional>
+#include <algorithm>
+using namespace std;
+int increment(int x)
+{
+    x = x + 1;
+    return (x);
+}
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int r[5];
+    transform(a, a + 5, r, increment);
+    for (int i = 0; i < 5; i++)
+        cout << r[i] << " ";
+    return (0);
+}
